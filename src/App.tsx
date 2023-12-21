1de73b4cc42ea551d1a187f72c4c3ce591dbf0a9
yerimgim <imyrgim92@gmail.com>
2023-12-21 22:45:21 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-// <reference path="path/types.d.ts" />
-import FaSun from "./assests/images/FaSun.png";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <h3>react-typescript boilerplate!</h3>
-      <h5>testing lint stage!!</h5>
-
-      <img src={FaSun} alt="FaSun" />
+      <h3>2023.12.21!</h3>
     </div>
   );
 };
